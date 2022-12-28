c31c084db2a5c720bb85ab4b997d7afaa5591ecf
vathymut <vathymut@gmail.com>
2022-12-28 03:40:35 -0500

    Update tests to use log-normal distribution.

--- tests/testthat/test-inliers-versus-outliers.R
+++ tests/testthat/test-inliers-versus-outliers.R
@@ -7,19 +7,19 @@ set.seed(123456)
 n <- 1e4
 alpha <- 0.05
 test_that("Inliers", {
-  tr <- rnorm(n, sd = 1)
-  te <- rnorm(n, sd = 1 / 2)
-  expect_lt(at_from_os(tr, te)$p_value, alpha)
+  tr <- rlnorm(n, sdlog = 1)
+  te <- rlnorm(n, sdlog = 1 / 2)
+  expect_gt(at_from_os(tr, te)$p_value, alpha)
 })
 
 test_that("Outliers", {
-  tr <- rnorm(n, sd = 1)
-  te <- rnorm(n, sd = 3 / 2)
+  tr <- rlnorm(n, sdlog = 1)
+  te <- rlnorm(n, sdlog = 3 / 2)
   expect_lt(at_from_os(tr, te)$p_value, alpha)
 })
 
 test_that("Same distribution", {
-  tr <- rnorm(n)
-  te <- rnorm(n)
+  tr <- rlnorm(n)
+  te <- rlnorm(n)
   expect_gt(at_from_os(tr, te)$p_value, alpha)
 })
